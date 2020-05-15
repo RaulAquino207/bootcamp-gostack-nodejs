109c1d8699321d51582e43e13becd5bc01af4a18
Raul Aquino <aquinoraul207@gmail.com>
2020-05-15 10:09:10 -0300

    Done simple refactoring in app.js, created controllers and models

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const RepoController = require('./controllers/RepoController');
+const LikeController = require('./controllers/LikeController');
 
-const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,23 +11,23 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.get("/repositories", (req, res) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
+app.post("/repositories", (req, res) => {
   // TODO
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", (req, res) => {
   // TODO
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+app.post("/repositories/:id/like", (req, res) => {
   // TODO
 });
 
